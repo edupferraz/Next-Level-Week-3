a63df7b598c0804c13c81917abf7065e5fa54743
edupferraz <edupferraz@gmail.com>
2020-10-13 10:30:52 -0300

    Dia 2 - Abstração

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
-
 import './database/connection'
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
 
 // Métodos HTTP = GETM POST, PUT, DELETE
 
@@ -18,38 +20,6 @@ app.use(express.json());
 // Query Params: http://localhost:3333/users?search=diego
 // Route Params: http://localhost:3333/users/1 (Identificar um recurso)
 // Body : http://localhost:3333/users (identificar um recurso)
-
-app.post('/orphanages', async (request, response) => {
-
-    const{
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours, 
-        open_on_weekends
-
-    } = request.body;
-
-    const orphanageRepository = getRepository(Orphanage);
-
-    const orphanage = orphanageRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours, 
-        open_on_weekends
-    });
-
-    await orphanageRepository.save(orphanage);
-
-    return response.status(201).json(orphanage);
-});
-
-app.listen(3333);
 
 // Driver nativo, Query builder, ORM -> Object Relational Mapping
 
