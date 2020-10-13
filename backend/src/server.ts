f8f3cf528a5d4261dde9473a651b1802bb908b2a
edupferraz <edupferraz@gmail.com>
2020-10-13 10:07:13 -0300

    Dia 2 - Banco de dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
+
+import './database/connection'
 
 const app = express();
 
@@ -15,13 +19,38 @@ app.use(express.json());
 // Route Params: http://localhost:3333/users/1 (Identificar um recurso)
 // Body : http://localhost:3333/users (identificar um recurso)
 
-app.post('/users/:id', (request, response) => {
+app.post('/orphanages', async (request, response) => {
 
-    console.log(request.query);
-    console.log(request.params);
-    console.log(request.body);
+    const{
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours, 
+        open_on_weekends
 
-    return response.send({ message: 'Hello World '});
+    } = request.body;
+
+    const orphanageRepository = getRepository(Orphanage);
+
+    const orphanage = orphanageRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours, 
+        open_on_weekends
+    });
+
+    await orphanageRepository.save(orphanage);
+
+    return response.status(201).json(orphanage);
 });
 
 app.listen(3333);
+
+// Driver nativo, Query builder, ORM -> Object Relational Mapping
+
+// users -> User ( Objeto )
